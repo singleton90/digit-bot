8275d0970d9d85c88f94da4cc0d1ca41dab6025a
Anton A <ant0ny@mail.ru>
2016-09-20 20:29:22 +0300

    new

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $telegram = new Api('202559226:AAEu2P5-OTOZMIeSQFDsEF4jizZu2rcTkHM');
 $response = $telegram->getUpdates();
 echo "<pre>";
 foreach ($response as $update) {
-	var_dump($update->message->from->username);
+	var_dump($update->getMessage());
 }
 echo "===<br>";
 var_dump($response);
