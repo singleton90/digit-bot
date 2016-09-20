feb3ecbc3fb080e76d08b4da063242c6bb2eb75d
Anton A <ant0ny@mail.ru>
2016-09-20 20:39:57 +0300

    new

--- index.php
+++ index.php
@@ -7,11 +7,12 @@ $telegram = new Api('202559226:AAEu2P5-OTOZMIeSQFDsEF4jizZu2rcTkHM');
 $response = $telegram->getUpdates();
 echo "<pre>";
 foreach ($response as $update) {
+	var_dump($update->getMessage()->getChat()->get('id'));
 	if ($update->getMessage()->getText() == '/start') {
-		$telegram->sendMessage([
-				'chat_id' => $update->getMessage()->getChat()->get('id'), 
-  				'text' => 'Hello World'
-			]);
+		// $telegram->sendMessage([
+		// 		'chat_id' => $update->getMessage()->getChat()->get('id'), 
+  // 				'text' => 'Hello World'
+		// 	]);
 	}
 }
 echo "===<br>";
