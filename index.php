47a80eb30cd1fd0e3a54c6d017578074af59eb88
Anton A <ant0ny@mail.ru>
2016-09-20 20:15:22 +0300

    new

--- index.php
+++ index.php
@@ -5,5 +5,9 @@ use Telegram\Bot\Api;
 $telegram = new Api('202559226:AAEu2P5-OTOZMIeSQFDsEF4jizZu2rcTkHM');
 
 $response = $telegram->getUpdates();
-
+echo "<pre>";
+foreach ($response as $update) {
+	var_dump($update);
+}
+echo "===<br>";
 var_dump($response);
